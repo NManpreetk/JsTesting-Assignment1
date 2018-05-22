b7d170a9516581c601b59de45519430f7f0ed30a
NManpreetk <37636920+NManpreetk@users.noreply.github.com>
2018-05-21 22:30:35 -0500

    Delete Duplicates.js

--- src/Duplicates.js
+++
@@ -1,6 +0,0 @@
-function removecharacters(st) {
-  return st.replace(st.charAt(st.length - 1), '').replace(st.charAt(0), '')
-  //     let myString = someString
-  //  let result = myString.slice(1, -1)
-  //  return result
-}
